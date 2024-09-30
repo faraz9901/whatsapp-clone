6f9d8a583dfa9755674abc28271fde5eadd824e9
faraz9901 <mdfaraz@sparkgenieit.com>
2024-09-30 21:20:17 +0530

    Testing

--- backend/index.ts
+++ backend/index.ts
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-const port = 5000;
+const port = 8000;
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Express + TypeScript Server');
